60a17d492d903d5157c61f339fb76707931af884
daw2-gil23 <gilalvarezmaria@fpllefia.com>
2022-11-03 20:54:24 +0100

    segundo commit

--- src/js/funciones/modificaData.js
+++ src/js/funciones/modificaData.js
@@ -3,18 +3,19 @@ export const modificaData = (tiempo) =>{
             //creamos un objeto del tipo Date con la fecha tiempo
             let tiempoActual = new Date(tiempo)
 
-            //devuelve el dia del objeto tiempoActual y la guardamos en dia
-            let dia = tiempoActual.getDate()
-            //devuelve el mes del objeto tiempoActual y la guardamos en mes
-            let mes = tiempoActual.getMonth()
             //devuelve el año del objeto tiempoActual y la guardamos en dia
             let anyo = tiempoActual.getFullYear()
-            //devuelve la hora del objeto tiempoActual y la guardamos en dia
-            let hora = tiempoActual.getHours()
-            //devuelve los minutos del objeto tiempoActual y la guardamos en dia
-            let minutos = tiempoActual.getMinutes()
-            //devuelve los segundos del objeto tiempoActual y la guardamos en dia
-            let segundos = tiempoActual.getSeconds()
+            //devuelve el mes del objeto tiempoActual y la guardamos en dia
+            let mes  = tiempoActual.getMonth()
+
+            //Hacemos esto para si el numero es menor a 10 se añada un 0 al principio
+            //El padstart lo que hace es añadirte en el valor que pongas en la longitud que le digas.
+            //Ejemplo: si es dia 1 saldar 01 pero si es dia 12 saldra 12 sin 0 
+            //Esto pasa por que hemos dicho que la longitud sea 2. El dia 1 es un numero asi que se rellanara con 0 en cambio El dia 12 son dos numeros asi que no hara nada
+            let dia = String(tiempoActual.getDate()).padStart(2, '0')
+            let hora = String(tiempoActual.getHours()).padStart(2, '0')
+            let min = String(tiempoActual.getMinutes()).padStart(2, '0')
+            let seg = String(tiempoActual.getSeconds() ).padStart(2, '0')
 
             //getMonth() devuelve el numero del dia que es. Para saber el nombre ahi que crear una array con los nombres de los meses
             const mesesNombres = ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio",
@@ -22,5 +23,5 @@ export const modificaData = (tiempo) =>{
             ];
 
             //Devolvemos los valores con la estructura dicha en el anunciado. Como el mes nos da el numero del mes que es, solamente ahi que ponerlo en la array para que coga de la array el numero que este en la variable mes
-            return(dia + " " + mesesNombres[mes] +   " - " + anyo + " " + hora + ":" + minutos + ":" + segundos);
+            return(dia + " " + mesesNombres[mes] +   " -  " + anyo + " " + hora + ":" + min + ":" + seg);
 }
--- src/js/funciones/modificaData2.js
+++ src/js/funciones/modificaData2.js
@@ -7,9 +7,18 @@ export const modificaData2 = (tiempo) =>{
             //creamos un objeto del tipo Date con la fecha tiempo
             let tiempoActual = new Date(tiempo)
 
+            //Hacemos esto para si el numero es menor a 10 se añada un 0 al principio
+            //El padstart lo que hace es añadirte en el valor que pongas en la longitud que le digas.
+            //Ejemplo: si es enero (1) saldar 01 pero si es diciembre(12) saldra 12 sin 0 
+            //Esto pasa por que hemos dicho que la longitud sea 2. Enero es un numero asi que se rellanara con 0 en cambio diciembre son dos numeros asi que no hara nada
+            let mes= String(tiempoActual.getMonth() + 1).padStart(2, '0')
+            let dia = String(tiempoActual.getDate()).padStart(2, '0')
+            let hora = String(tiempoActual.getHours()).padStart(2, '0')
+            let min = String(tiempoActual.getMinutes()).padStart(2, '0')
+            let seg = String(tiempoActual.getSeconds() ).padStart(2, '0')
+
             //Cogemos los valores y los ponemos en el formato que ha dicho el anunciado
-            let tiempoActualFormato = tiempoActual.getFullYear() + "/" + (tiempoActual.getMonth() + 1) + "/" + tiempoActual.getDate() + " " + tiempoActual.getHours() + ":" + tiempoActual.getMinutes() + ":" + tiempoActual.getSeconds() 
-            console.log(tiempoActualFormato)
+            let tiempoActualFormato = tiempoActual.getFullYear() + "/" + mes + "/" + dia + " " + hora + ":" + min + ":" + seg
 
             //Devolvemos la fecha en la forma del anunciado
             return(tiempoActualFormato)
